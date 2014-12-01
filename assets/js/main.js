685888606bd2baf443eb6cfff3c69f491dceeaab
Markus Deutschl <deutschl.markus@gmail.com>
2014-12-01 19:32:07 +0100

    Enhanced mobile header menu.

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function (){
+  // Toggle mobile menu.
+  $('#mobile-menu').click(function (ev) {
+    ev.preventDefault();
+    var $header = $('#header');
+    var $mobileMenu = $('#mobile-menu')
+    if ($header.hasClass('expanded')) {
+      $header.removeClass('expanded');
+      $mobileMenu.removeClass('expanded');
+    }
+    else {
+      $header.addClass('expanded');
+      $mobileMenu.addClass('expanded');
+    }
+    return false;
+  });
+});
